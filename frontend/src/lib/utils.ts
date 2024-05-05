c8c41c2e46be7415aedd21e2da89d3759a819597
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-05-05 13:01:06 +0530

    feat: typo in utils.ts
    Signed-off-by: Tanmay Sarkar <sarkartanmay393@gmail.com>

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -6,10 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const BACKEND_URL =
-  process.env.NODE_ENV !== "development"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
     : "https://zvg2nyusw6.execute-api.ap-south-1.amazonaws.com/dev";
 export const BACKEND_API_URL =
-  process.env.NODE_ENV !== "development"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:8080/api"
     : "https://zvg2nyusw6.execute-api.ap-south-1.amazonaws.com/dev/api";
